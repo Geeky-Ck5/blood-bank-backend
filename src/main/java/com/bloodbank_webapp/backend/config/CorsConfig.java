d1ee893e2e8c943da88a22a07b439dd843f25186
Kavish Seechurn <mytkavish@hotmail.com>
2025-03-03 05:26:59 +0400

    updated CORS Setting to get request from FRONTEND v1

--- src/main/java/com/bloodbank_webapp/backend/config/CorsConfig.java
+++ src/main/java/com/bloodbank_webapp/backend/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bloodbank-api-csbxgsaabfc4bjbn.eastus-01.azurewebsites.net") // Add localhost:4200 if testing from frontend
+                        .allowedOrigins("https://proud-bush-0a6644c0f.4.azurestaticapps.net") // Add localhost:4200 if testing from frontend
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
